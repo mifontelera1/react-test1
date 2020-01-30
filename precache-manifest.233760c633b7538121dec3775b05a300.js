0c6eb491d7c289a2f81e9aec43b3c2ef42ab2048
mifontelera1 <mifontelera1@gmail.com>
2020-01-30 11:07:43 +0800

    Updates

---
+++ precache-manifest.233760c633b7538121dec3775b05a300.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f81eb7a47c939016f0857aa07acca4c9",
+    "url": "/react-test1/index.html"
+  },
+  {
+    "revision": "79e78622f29cb0aff8cc",
+    "url": "/react-test1/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8af1c97d36f0b1426ae8",
+    "url": "/react-test1/static/js/2.b6fabe4d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-test1/static/js/2.b6fabe4d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "79e78622f29cb0aff8cc",
+    "url": "/react-test1/static/js/main.39a70680.chunk.js"
+  },
+  {
+    "revision": "dc31a80e11ba3e51ff98",
+    "url": "/react-test1/static/js/runtime-main.34ce02c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-test1/static/media/logo.5d5d9eef.svg"
+  }
+]);
